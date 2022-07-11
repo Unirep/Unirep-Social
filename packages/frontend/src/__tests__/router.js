fb96816848d174b98f435bbd28ed153e4a7a3757
Anthony Madia <aamadia24@gmail.com>
2022-07-11 19:47:19 -0400

    Comment out log

--- packages/frontend/src/__tests__/router.js
+++ packages/frontend/src/__tests__/router.js
@@ -3,9 +3,9 @@ import AppRouter from '../router'
 
 // Is there a better solution than this?:  https://github.com/facebook/jest/issues/10784#:~:text=NODE_OPTIONS%3D%2D%2Dunhandled%2Drejections%3Dwarn%20yarn%20test
 
-process.on('unhandledRejection', (reason, promise) => {
-  console.log('unhandledRejection', reason, promise);
-});
+// process.on('unhandledRejection', (reason, promise) => {
+//   console.log('unhandledRejection', reason, promise);
+// });
   
 
 test('Renders AppRouter', () => {
