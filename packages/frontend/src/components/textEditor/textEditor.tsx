4aec3a5e4526b8fe23ad89607c34fd64d8caf523
yu-zhen <wlphg010688@gmail.com>
2022-07-12 21:50:49 +0800

    add bold and italic styling

--- packages/frontend/src/components/textEditor/textEditor.tsx
+++ packages/frontend/src/components/textEditor/textEditor.tsx
@@ -1,9 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import MarkdownIt from 'markdown-it'
 
 type Props = {
     content?: string
     setContent: (value: string) => void
+}
+
+enum TextStyle {
+    Bold,
+    Italic
 }
 
 const markdown = new MarkdownIt({
@@ -20,6 +25,23 @@ const TextEditor = ({ content, setContent }: Props) => {
         setContent(event.target.value)
     }
 
+    const addStyle = (style: TextStyle) => {
+        const sel = document.getElementById('myTextArea') as HTMLTextAreaElement
+        const start = sel.selectionStart
+        const end = sel.selectionEnd
+        console.log('selection start from ', start, ' to ', end)
+
+        if (content) {
+            if (style === TextStyle.Italic) {
+                const newContent = content.substring(0, start) + '*' + content.substring(start, end) + '*' + content.substring(end)
+                setContent(newContent)
+            } else if (style === TextStyle.Bold) {
+                const newContent = content.substring(0, start) + '**' + content.substring(start, end) + '**' + content.substring(end)
+                setContent(newContent)
+            }
+        }
+    }
+
     return (
         <div>
             <button onClick={() => setIsPreview(!isPreview)}>preview</button>
@@ -30,7 +52,13 @@ const TextEditor = ({ content, setContent }: Props) => {
                 }}
             />
             ) : (
-                <textarea onChange={handleContentInput} value={content ?? ''} />
+                <div>
+                    <div className="buttons">
+                        <button onClick={() => addStyle(TextStyle.Bold)}>Bold</button>
+                        <button onClick={() => addStyle(TextStyle.Italic)}>Italic</button>
+                    </div>
+                    <textarea id="myTextArea" onChange={handleContentInput} value={content ?? ''} />
+                </div>
             )}
         </div>
     )
