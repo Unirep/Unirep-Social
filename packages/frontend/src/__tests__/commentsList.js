99122e6aade1e846b853021cfe9db764798f0221
Anthony Madia <aamadia24@gmail.com>
2022-07-28 15:53:29 -0400

    Fix: commentBlock test with better assertions and mocked data

--- packages/frontend/src/__tests__/commentsList.js
+++ packages/frontend/src/__tests__/commentsList.js
@@ -1,37 +1,65 @@
 import { screen, render, waitFor } from '@testing-library/react'
 import CommentsList from '../components/postsList/commentsList'
+import PostContext from '../context/Post'
 
-// mocked props
-const commentIds = ['1', '2', '3']
-const page = '/post'
-const mockLoadMoreComments = jest.fn()
-
-test('should render CommentLists correctly *without* comments', () => {
-    render(
-        <CommentsList
-            commentIds={0}
-            page={page}
-            loadMoreComments={mockLoadMoreComments}
-        />
+const renderCommentsField = (postData, commentIds, page, loadMoreComments) => {
+    return render(
+        <PostContext.Provider value={postData}>
+            <CommentsList
+                commentIds={commentIds}
+                page={page}
+                loadMoreComments={loadMoreComments}
+            />
+        </PostContext.Provider>
     )
-    expect(screen.getByText(/it's empty here./i)).toBeInTheDocument()
-    expect(
-        screen.getByText(/people just being shy, no post yet./i)
-    ).toBeInTheDocument()
-    expect(screen.queryByText(/load more posts/i)).not.toBeInTheDocument()
+}
+
+test('should render CommentLists correctly with data and populated commentIds prop', () => {
+    const commentIds = ['1']
+    const page = '/post'
+
+    const postData = {
+        commentsById: {
+            1: {
+                id: '1',
+                content: 'content from commentsById',
+                createdAt: '00',
+                reputation: 30,
+                epoch_key: 'epoch_key test',
+            },
+        }
+    }
+
+
+    renderCommentsField(postData, commentIds, page, jest.fn())
+    expect(screen.getByText(/post by/i)).toBeInTheDocument()
+    expect(screen.getByText(/epoch_key test/i)).toBeInTheDocument()
+    expect(screen.getByText(/etherscan/i)).toBeInTheDocument()
+    expect(screen.getByText(/content from commentsById/i)).toBeInTheDocument()
+    expect(screen.getByText(/boost/i)).toBeInTheDocument()
+    expect(screen.getByText(/squash/i)).toBeInTheDocument()
+    expect(screen.getByText(/share/i)).toBeInTheDocument()
 })
 
-test.skip('should render CommentLists commentBlocks with commentIds array > 0', () => {
-    render(
-        <CommentsList
-            commentIds={commentIds}
-            page={page}
-            loadMoreComments={mockLoadMoreComments}
-        />
-    )
+test('should render CommentLists with nothing in the commentIds array', () => {
+    // commentIds is an empty array
+    const commentIds = []
+    const page = '/post'
+
+    const postData = {
+        commentsById: {
+            1: {
+                id: '1',
+                content: 'content from commentsById',
+                createdAt: '00',
+                reputation: 30,
+                epoch_key: 'epoch_key test',
+            },
+        }
+    }
+
+
+    renderCommentsField(postData, commentIds, page, jest.fn())
     expect(screen.getByText(/it's empty here./i)).toBeInTheDocument()
-    expect(
-        screen.getByText(/people just being shy, no post yet./i)
-    ).toBeInTheDocument()
-    expect(screen.queryByText(/load more posts/i)).not.toBeInTheDocument()
+    expect(screen.getByText(/people just being shy, no post yet./i)).toBeInTheDocument()
 })
