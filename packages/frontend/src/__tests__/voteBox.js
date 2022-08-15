ea627ec9e7e084b7adce43466541d562b413a18c
yu-zhen <wlphg010688@gmail.com>
2022-08-15 16:20:13 +0800

    fix: fix test import

--- packages/frontend/src/__tests__/voteBox.js
+++ packages/frontend/src/__tests__/voteBox.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import UserContext from '../context/User'
 import PostContext from '../context/Post'
-import VoteBox from '../components/voteBox/voteBox'
+import VoteBox from '../components/voteBox'
 
 const renderVoteBox = (
     userData,
