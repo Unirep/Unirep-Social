f57c5e6b93939d3f82564a43d78d264ef0efc46e
Anthony Madia <aamadia24@gmail.com>
2022-09-09 14:57:40 -0400

    Fix(frontend): better test to simulate using typing in textEditor

--- packages/frontend/src/__tests__/textEditor.js
+++ packages/frontend/src/__tests__/textEditor.js
@@ -1,22 +1,36 @@
-import { screen, render } from '@testing-library/react'
+import { screen, render, waitFor } from '@testing-library/react'
+import { useState } from 'react'
 import userEvent from '@testing-library/user-event'
 import TextEditor from '../components/textEditor'
 
 const loremText =
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
 
-const renderTextEditor = (content, setContent, autoFocus) => {
-    return render(
+const TestWrapper = () => {
+    const [content, setContent] = useState('')
+    return (
         <TextEditor
             content={content}
             setContent={setContent}
-            autoFocus={autoFocus}
+            autoFocus={true}
         />
     )
 }
+
+const renderTextEditor = () => {
+    return render(<TestWrapper />)
+}
+
 test('type text into textbox', async () => {
-    renderTextEditor(loremText, jest.fn(), true)
-    expect(screen.getByText(loremText)).toBeInTheDocument()
+    renderTextEditor()
+    const textbox = screen.getByRole('textbox')
+    userEvent.type(textbox, loremText)
+    await waitFor(
+        () => {
+            expect(textbox).toHaveValue(loremText)
+        },
+        { timeout: 10000 }
+    )
 })
 
 test('should toggle the Preview and Edit functionality', async () => {
