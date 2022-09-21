23627bbf6d40f596cc6a59a381a23b4e2af3bf4d
yu-zhen <wlphg010688@gmail.com>
2022-09-21 13:12:57 -0400

    feat: modify setting page, header, userpage which related to setting page

--- packages/frontend/src/pages/settingPage/settingPage.tsx
+++ packages/frontend/src/pages/settingPage/settingPage.tsx
@@ -1,23 +1,47 @@
-import { useHistory, useLocation } from 'react-router-dom'
+import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
 import BasicPage from '../basicPage/basicPage'
 import PrivateKey from './privateKey'
 
+enum SettingNav {
+    PrivateKey,
+    Username,
+}
+
 const SettingPage = () => {
-    const history = useHistory()
     const location = useLocation<Location>()
     const state = JSON.parse(JSON.stringify(location.state))
     const isConfirmed = state.isConfirmed
 
+    const [nav, setNav] = useState<SettingNav>(SettingNav.PrivateKey)
+
     return (
         <BasicPage>
-            <div
-                className="back"
-                onClick={() => history.push('/user', { isConfirmed: true })}
-            >
-                <img src={require('../../../public/images/arrow-left.svg')} />
+            <h3>Settings</h3>
+            <div className="setting-nav-bar">
+                <div
+                    className={
+                        nav === SettingNav.PrivateKey
+                            ? 'setting-nav chosen'
+                            : 'setting-nav'
+                    }
+                    onClick={() => setNav(SettingNav.PrivateKey)}
+                >
+                    Private Key
+                </div>
+                <div className="interline"></div>
+                <div
+                    className={
+                        nav === SettingNav.Username
+                            ? 'setting-nav chosen'
+                            : 'setting-nav'
+                    }
+                    onClick={() => setNav(SettingNav.Username)}
+                >
+                    User Name
+                </div>
             </div>
-            <PrivateKey />
         </BasicPage>
     )
 }
