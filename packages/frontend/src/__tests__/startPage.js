27b0a3629a1712f5c66b6059b5377c82caafbc90
yu-zhen <wlphg010688@gmail.com>
2022-09-22 19:55:59 -0400

    update tests

--- packages/frontend/src/__tests__/startPage.js
+++ packages/frontend/src/__tests__/startPage.js
@@ -15,17 +15,15 @@ jest.mock('react-router-dom', () => ({
 
 test('should render the startPage properly', () => {
     const p1 =
-        'Great to have you here. Currently, UniRep Social is an experimental & research use dApp. We are part of the Privacy & Scaling Explorations team that specializes in zero-knowledge proof and advanced blockchain technology.'
-    const p2 =
-        'Our mission is to empower the general public to have full privacy under the social media setup, while earning the reputation they deserved. It’s tricky, but yes, we know it’s very important.'
+        '👋​ Great to have you here, Please select one of following to start. We only use your social ID to generate an identity proof, you are fully anonymous here.'
     const note =
         'If you have previously used UniRep, you might need to re-sign up again, since we have change the network.'
     render(<StartPage />)
     expect(
         screen.getByText(p1) &&
-            screen.getByText(p2) &&
             screen.getByText(note) &&
             screen.getByText('Sign In') &&
-            screen.getByText('Sign Up')
+            screen.getByText('Twitter') &&
+            screen.getByText('Github')
     ).toBeInTheDocument()
 })
