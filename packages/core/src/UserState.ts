60c55c98e25dbcbdcbe309176c7e241e1e7a139d
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2022-10-09 23:44:59 -0400

    feat(core): specify epoch of the epoch key proof

--- packages/core/src/UserState.ts
+++ packages/core/src/UserState.ts
@@ -1,8 +1,46 @@
+import assert from 'assert'
 import { UserState } from '@unirep/core'
-import { BaseProof } from '@unirep/contracts'
+import { BaseProof, EpochKeyProof } from '@unirep/contracts'
 import { stringifyBigInts } from '@unirep/crypto'
+import { Circuit } from '@unirep/circuits'
 
 export class SocialUserState extends UserState {
+    public genVerifyEpochKeyProof = async (
+        epochKeyNonce: number,
+        _epoch?: number
+    ): Promise<EpochKeyProof> => {
+        assert(
+            epochKeyNonce < this.settings.numEpochKeyNoncePerEpoch,
+            `epochKeyNonce (${epochKeyNonce}) must be less than max epoch nonce`
+        )
+        const epoch = _epoch ?? (await this.latestTransitionedEpoch())
+        const leafIndex = await this.latestGSTLeafIndex()
+        const userStateTree = await this.genUserStateTree(epoch)
+        const GSTree = await this.genGSTree(epoch)
+        const GSTProof = GSTree.createProof(leafIndex)
+
+        const circuitInputs = stringifyBigInts({
+            GST_path_elements: GSTProof.siblings,
+            GST_path_index: GSTProof.pathIndices,
+            identity_nullifier: this.id.identityNullifier,
+            identity_trapdoor: this.id.trapdoor,
+            user_tree_root: userStateTree.root,
+            nonce: epochKeyNonce,
+            epoch: epoch,
+        })
+
+        const results = await this.prover.genProofAndPublicSignals(
+            Circuit.verifyEpochKey,
+            circuitInputs
+        )
+
+        return new EpochKeyProof(
+            results.publicSignals,
+            results.proof,
+            this.prover
+        )
+    }
+
     async genSubsidyProof(
         attesterId: BigInt,
         minRep: BigInt = BigInt(0),
