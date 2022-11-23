0b7c005b2d039eca5047d5e7ed5dddb5385b9632
AnthonyMadia <aamadia24@gmail.com>
2022-11-23 12:29:07 -0500

    check CI

--- packages/frontend/src/layout/sideColumn/privateKeyDetail.tsx
+++ packages/frontend/src/layout/sideColumn/privateKeyDetail.tsx
@@ -1,7 +1,7 @@
 const PrivateKeyDetail = () => {
     return (
         <div className="widget private-key-detail">
-            <h4>What’s private key?</h4>
+            <h4>What’s the private key?</h4>
             <p>
                 The private key in UniRep social is actually the Semaphore
                 identity. Semaphore enable users online to be freely and fully
