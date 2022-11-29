d5da4b99810cb09d937c8642fc27d1253e5b01f5
AnthonyMadia <aamadia24@gmail.com>
2022-11-29 14:08:20 -0500

    add: unit test edit page

--- packages/frontend/src/__tests__/editPage.js
+++ packages/frontend/src/__tests__/editPage.js
@@ -1,19 +1,23 @@
 import { screen, render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import EditPage from '../pages/editPage/editPage'
+import { DELETED_CONTENT } from '../config'
 
 import PostContext from '../context/Post'
 
-// mock needed for history and location hooks
+// mock needed for history and params hooks
 const mockHistoryPush = jest.fn()
 
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
     useHistory: () => ({
         push: mockHistoryPush,
+        goBack: mockHistoryPush,
     }),
     useParams: jest.fn().mockReturnValue({ id: '1' }),
 }))
+
+// const historyGoBackSpy = jest.spyOn(react-router-dom, 'goBack')
 
 const renderEditPage = (postData) => {
     return render(
@@ -23,12 +27,12 @@ const renderEditPage = (postData) => {
     )
 }
 
-test('render editPage component', () => {
+test('initial render with truthy PostContext data', async () => {
     const postData = {
         postsById: {
             1: {
                 id: 'post id',
-                content: 'content from 0102992849301',
+                content: 'post content',
                 createdAt: new Date().toUTCString(),
                 reputation: 30,
                 epoch_key: 'epoch_key',
@@ -38,5 +42,23 @@ test('render editPage component', () => {
         deletePost: jest.fn(),
     }
     renderEditPage(postData)
-    screen.debug()
+    expect(
+        screen.getAllByText('Update Post') &&
+            screen.getByText('post content') &&
+            screen.getByText('Delete Post')
+    ).toBeInTheDocument()
+    const deletePost = screen.getByText('Delete Post')
+    await deletePost.click()
+    // should render delete post prompt after click
+    screen.debug(deletePost)
+    expect(
+        screen.getByText('Are you sure to delete this post?') &&
+            screen.getByText('Nevermind.') &&
+            screen.getByText('Yes, delete it.')
+    ).toBeInTheDocument()
+    // Grab delete button in prompt and click it
+    const yesDeletePost = screen.getByText('Yes, delete it.')
+    await yesDeletePost.click()
 })
+
+// todo: expect that the useHistory and useParams react hooks are being called
