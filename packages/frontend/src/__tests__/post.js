ed44280a289bd59cf9bf0d75941b67e99e5d3bd7
AnthonyMadia <aamadia24@gmail.com>
2022-12-06 13:11:53 -0500

    fix: refactor feedKey tests

--- packages/frontend/src/__tests__/post.js
+++ packages/frontend/src/__tests__/post.js
@@ -1,6 +1,20 @@
 import { Data } from '../context/Post'
+import fetch from 'node-fetch'
+import UnirepContext, { UnirepConfig } from '../context/Unirep'
+
+const unirepConfig = UnirepContext._currentValue
 
 let post
+
+jest.mock('node-fetch', () => ({
+    __esModule: true,
+    default: jest.fn(),
+    BigInt: jest.fn(),
+}))
+
+afterEach(() => {
+    jest.clearAllMocks()
+})
 
 describe('Post', function () {
     beforeEach(() => {
@@ -32,10 +46,57 @@ describe('Post', function () {
             content: 'Test comment content',
         })
     })
-    test('save() method calls localStorage', () => {
-        const windowSetItemSpy = jest.spyOn(localStorage, 'setItem')
+    test('saves the post and comment drafts to local storage', () => {
+        post.postDraft = { title: 'Test Post', content: 'Test content' }
+        post.commentDraft = {
+            title: 'Test Comment',
+            content: 'Test comment content',
+        }
+
         post.save()
-        expect(windowSetItemSpy).toHaveBeenCalledTimes(2)
+
+        expect(window.localStorage.getItem('post-draft')).toBe(
+            '{"title":"Test Post","content":"Test content"}'
+        )
+        expect(window.localStorage.getItem('comment-draft')).toBe(
+            '{"title":"Test Comment","content":"Test comment content"}'
+        )
+    })
+    test('converts an epoch key to a hex string', () => {
+        const epochKey = '12345678'
+        const hexString = post.convertEpochKeyToHexString(epochKey)
+
+        expect(hexString).toBe('bc614e')
+    })
+
+    test.skip('loads a single post and ingests it', async () => {
+        // mock fetch API
+        const postData = {
+            id: 'test-id',
+            title: 'Test Post',
+            content: 'Test content',
+        }
+        const r = { json: () => postData }
+        fetch.mockReturnValue(Promise.resolve(r))
+
+        // mock unirepConfig.loadingPromise
+        unirepConfig.loadingPromise = Promise.resolve()
+
+        // mock convertDataToPost
+        const convertDataToPostSpy = jest.spyOn(post, 'convertDataToPost')
+        convertDataToPostSpy.mockImplementation(() => postData)
+
+        // mock ingestPosts
+        const ingestPostsSpy = jest.spyOn(post, 'ingestPosts')
+
+        await post.loadPost('test-id')
+
+        expect(convertDataToPostSpy).toHaveBeenCalledWith(postData)
+        expect(ingestPostsSpy).toHaveBeenCalledWith(postData)
+
+        // clean up
+        convertDataToPostSpy.mockRestore()
+        ingestPostsSpy.mockRestore()
     })
 
     test.skip('loadPost() functionality', async () => {
@@ -44,9 +105,24 @@ describe('Post', function () {
         await post.loadPost('idstring')
     })
 
-    test('feedKey() returns correct string', () => {
-        const value = post.feedKey('query', ['epk'])
-        expect(value).toBe('query-user')
+    test('returns the query when epks are empty', () => {
+        const query = 'test-query'
+        const epks = []
+        const expectedKey = query
+
+        const key = post.feedKey(query, epks)
+
+        expect(key).toBe(expectedKey)
+    })
+
+    test('returns the query and user when epks are not empty', () => {
+        const query = 'test-query'
+        const epks = ['test-epk']
+        const expectedKey = `${query}-user`
+
+        const key = post.feedKey(query, epks)
+
+        expect(key).toBe(expectedKey)
     })
     // TODO: undefined to a BigInt error
     test.skip('loadPost() calls injestPosts', async () => {
