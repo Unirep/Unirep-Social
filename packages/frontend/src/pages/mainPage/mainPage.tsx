8abfb66ca67b31fa3707bdb09799fa398023fdbc
AnthonyMadia <aamadia24@gmail.com>
2022-12-13 11:17:30 -0500

    fix: loadMorePosts logic

--- packages/frontend/src/pages/mainPage/mainPage.tsx
+++ packages/frontend/src/pages/mainPage/mainPage.tsx
@@ -6,12 +6,10 @@ import PostContext from '../../context/Post'
 import UserContext from '../../context/User'
 import UnirepContext from '../../context/Unirep'
 
-import { QueryType, AlertType } from '../../constants' 
+import { QueryType, AlertType } from '../../constants'
 import BasicPage from '../basicPage/basicPage'
 import PostsList from '../../components/postsList'
 import Feed from '../../components/feed'
-
-// todo: the logic will be refactored to call `postContext.loadFeedByTopic`
 
 const MainPage = () => {
     const history = useHistory()
@@ -21,28 +19,37 @@ const MainPage = () => {
     const unirepConfig = useContext(UnirepContext)
 
     const [query, setQuery] = useState<QueryType>(QueryType.New)
-    const [topic, setTopic] = useState('')
-
-    const loadMorePosts = () => {
-        console.log(
-            'load more posts, now posts: ' +
-                postContext.feedsByQuery[query]?.length
-        )
-        postContext.loadFeed(query, postContext.feedsByQuery[query])
-    }
+    const [topic, setTopic] = useState('') // used for `goToNewPost` function only
 
     useEffect(() => {
         const pathname = location.pathname // will be '/topic'
-        const topic = pathname.split('/')[1] // this will be 'topic'
-        setTopic(topic)
+        const topicName = pathname.split('/')[1] // this will be 'topic'
+        setTopic(topicName)
+        loadMorePosts(topicName)
     }, [])
+
+    const loadMorePosts = (topic: any) => {
+        // console.log(
+        //     'load more posts, now posts: ' +
+        //         postContext.feedsByTopic[topic]?.length
+        // )
+        if (topic.charAt(0) === '') {
+            console.log('query in the undefined topic logic:', query)
+            postContext.loadFeed(query, postContext.feedsByQuery[query] || [])
+        } else {
+            postContext.loadFeedByTopic(
+                topic,
+                postContext.feedsByTopic[topic] || []
+            )
+        }
+    }
 
     const gotoNewPost = () => {
         if (
             userContext.userState &&
             userContext.spendableReputation >= unirepConfig.postReputation
         ) {
-            // history.push('/new', { isConfirmed: true })
+            // pass topic state to new page
             history.push(
                 {
                     pathname: '/new',
