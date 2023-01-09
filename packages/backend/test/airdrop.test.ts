b0144021d8fabbaa0966ee35cc09fa4daafa0c06
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2023-01-09 15:09:36 +0800

    test(backend): add test for subsidy

--- packages/backend/test/airdrop.test.ts
+++ packages/backend/test/airdrop.test.ts
@@ -61,3 +61,43 @@ test('should get negative reputation airdrop', async (t: any) => {
     await createPostSubsidy(t, user1.iden)
     t.pass()
 })
+
+test('should not claim airdrop twice', async (t: any) => {
+    // sign up first user
+    const user1 = await signUp(t)
+
+    // first create a post
+    const { post } = await createPost(t, user1.iden)
+
+    // sign up second user
+    const user2 = await signUp(t)
+
+    // downvote the post
+    const upvote = 0
+    const downvote = 7
+    const receiver = post.epochKey.toString()
+    await vote(t, user2.iden, receiver, post._id, true, upvote, downvote)
+
+    // user state transition
+    {
+        await epochTransition(t)
+        // user state transition
+        await userStateTransition(t, user1.iden)
+    }
+
+    // user prove airdrop
+    const postReputation = (
+        await t.context.unirepSocial.postReputation()
+    ).toNumber()
+    await airdrop(t, user1.iden, postReputation + downvote - airdropReputation)
+    try {
+        await airdrop(
+            t,
+            user1.iden,
+            postReputation + downvote - airdropReputation
+        )
+        t.fail()
+    } catch (_) {
+        t.pass()
+    }
+})
